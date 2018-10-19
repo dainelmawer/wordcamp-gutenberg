cdcacc47a0b3dd8f4a7ed2603e8ee53a1cc32d37
dainemawer <daine.mawer@10up.com>
2018-10-19 14:03:37 +0200

    Scaffolded plugin

---
+++ gutenworkshop.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Gutenworkshop
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     gutenworkshop
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Gutenworkshop
+ */
+
+// Your code starts here.
